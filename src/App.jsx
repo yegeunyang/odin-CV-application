73464b6ed5df26a5c6986eb169aa4809e123eba1
yegeunyang <yegeunyang@icloud.com>
2025-01-10 10:45:43 -0600

    Split in half and add background color to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)"}}>
+      <div className="editor" style={{ backgroundColor: "white", width: "50vw" }}>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="viewer" style={{ backgroundColor: "grey", width: "50vw", height: "100vh" }}>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
